91ea1e956d5705576b55fcd2a2f5102c7a5f25e9
babxiluca <alirsali@gmail.com>
2023-10-04 11:54:06 -0600

    10

--- main.tf
+++ main.tf
@@ -8,21 +8,21 @@ module "vpc" {
 
   name = "vpc-${var.environment}"
   cidr = var.cidr_block
-  
 
-  azs = local.availability_zones
-  private_subnets = [cidrsubnet(var.cidr_block, 3, 0),cidrsubnet(var.cidr_block, 3, 1)]
-  private_subnet_tags =  { Name = "private"}
-  public_subnets = [cidrsubnet(var.cidr_block, 3, 3), cidrsubnet(var.cidr_block, 3, 4)]
-  public_subnet_tags = { Name = "public"}
 
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  enable_nat_gateway = true
-  one_nat_gateway_per_az = true
-  create_igw = false
+  azs                 = local.availability_zones
+  private_subnets     = [cidrsubnet(var.cidr_block, 3, 0), cidrsubnet(var.cidr_block, 3, 1)]
+  private_subnet_tags = { Name = "private" }
+  public_subnets      = [cidrsubnet(var.cidr_block, 3, 3), cidrsubnet(var.cidr_block, 3, 4)]
+  public_subnet_tags  = { Name = "public" }
+
+  enable_dns_support            = true
+  enable_dns_hostnames          = true
+  enable_nat_gateway            = true
+  one_nat_gateway_per_az        = true
+  create_igw                    = false
   manage_default_security_group = false
- 
+
   tags = {
     Terraform   = "true"
     Environment = "dev"
